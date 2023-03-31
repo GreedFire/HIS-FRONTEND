e2f1ef90446fd40491f41a1b82a60763b5f7f1cb
Dawid Majchrzak <dawid.majchrzak@szpital.glogow>
2023-03-31 08:08:53 +0200

    first commit

---
+++ src/main/java/com/hisfrontend/HisFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.hisfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HisFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HisFrontendApplication.class, args);
+    }
+
+}
